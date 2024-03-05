7f8371e3ec655193376f362ab63584d8c3efe2f2
Sunny Eyles <sunnyeyles@Sunnys-MacBook-Air.local>
2024-03-05 19:05:06 +0400

    Bug fixed

--- server/api/s3/purge_bucket.py
+++ server/api/s3/purge_bucket.py
@@ -14,22 +14,14 @@ s3 = boto3.client(
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
 )
-S3_BUCKET_NAME = "why-are-there-so-many-bucket-naming-rules"  
+S3_BUCKET_NAME = "why-are-there-so-many-bucket-naming-rules"
 
 def purge_bucket():
     # print(s3.list_objects(Bucket=S3_BUCKET_NAME))
     all_objects = s3.list_objects(Bucket=S3_BUCKET_NAME)
     first_item_key, first_item_value = next(iter(all_objects.items()))
     for object in all_objects:
-        # print(object)
-        first_item_key, first_item_value = next(iter(object.items()))
-        
-        print(first_item_key, first_item_value)
-        
-    # request_id = all_objects['ResponseMetadata']['RequestId']
-
-
-
+        print(first_item_key, first_item_value)        
 
     
 if __name__ == "__main__":
